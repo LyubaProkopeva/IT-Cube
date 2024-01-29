57f9b1084cb5a85204ae411622166671f07718f1
LyubaProkopeva <157283910+LyubaProkopeva@users.noreply.github.com>
2024-01-29 22:23:53 +0800

    Add files via upload

---
+++ Урок 3 Переменные. Элементарные действия с числами./task2.py
@@ -0,0 +1,10 @@
+# Задание №2
+name1 = int(input())
+name2 = int(input())
+print(name1 + name2)
+print(name1 - name2)
+print(name1 * name2)
+print(name1 / name2)
+print(name1 // name2)
+print(name1 % name2) 
+print(name1 ** name2)
